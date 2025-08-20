a4c433787176d663decbd88c3b08333a6d852e10
mariva0280 <marivafp@gmail.com>
2025-08-20 11:09:22 +0200

    implement check-collision logic in space-invaders #5

--- staff/vanessa-fernandez/playground/space-invaders/src/index.ts
+++ staff/vanessa-fernandez/playground/space-invaders/src/index.ts
@@ -15,7 +15,7 @@ const constants = {
         dimensions: { width: 50, height: 30 }
     },
     enemy: {
-        dimensions: { width: 40, height: 20 }
+        dimensions: { width: 40, height: 40 }
     },
     bullets: {
         dimensions: { width: 5, height: 10 }
@@ -30,7 +30,7 @@ const state = {
     lives: 3,       
     level: 1,
     difficulty: 'easy',
-    spaceship: { 
+    player: { 
         position: { x: 0, y: 0 }
     },
     enemies: [{
@@ -62,4 +62,31 @@ Logic
 - reset game
 */
 
+function checkCollision() {
+    const playerHalfWidth = constants.player.dimensions.width / 2;
+    const playerHalfHeight = constants.player.dimensions.height / 2;    
+    const enemyHalfWidth = constants.enemy.dimensions.width / 2;
+    const enemyHalfHeight = constants.enemy.dimensions.height / 2;
 
+    const playerVertices = {
+        topLeft: { x: state.player.position.x - playerHalfWidth, y: state.player.position.y + playerHalfHeight },
+        topRight: { x: state.player.position.x + playerHalfWidth, y: state.player.position.y + playerHalfHeight },
+        bottomRight: { x: state.player.position.x + playerHalfWidth, y: state.player.position.y - playerHalfHeight },
+        bottomLeft: { x: state.player.position.x - playerHalfWidth, y: state.player.position.y - playerHalfHeight }
+    }
+
+    return state.enemies.some((enemy) => {
+        const enemyVertices = {
+            topLeft: { x: enemy.position.x - enemyHalfWidth, y: enemy.position.y + enemyHalfHeight },
+            topRight: { x: enemy.position.x + enemyHalfWidth, y: enemy.position.y + enemyHalfHeight },
+            bottomRight: { x: enemy.position.x + enemyHalfWidth, y: enemy.position.y - enemyHalfHeight },
+            bottomLeft: { x: enemy.position.x - enemyHalfWidth, y: enemy.position.y - enemyHalfHeight }
+        }
+
+        return playerVertices.topLeft.x <= enemyVertices.bottomRight.x &&
+                playerVertices.topRight.x >= enemyVertices.topLeft.x &&
+                playerVertices.topLeft.y >= enemyVertices.bottomRight.y &&
+                playerVertices.bottomRight.y <= enemyVertices.topLeft.y
+        
+    })
+}
