8d8b11453794699782e2211f126e0ab6753ec48e
mariva0280 <marivafp@gmail.com>
2025-09-02 19:14:03 +0200

    add bullets to game and detect collisions with invaders; #5

--- staff/vanessa-fernandez/playground/space-invaders/src/index.ts
+++ staff/vanessa-fernandez/playground/space-invaders/src/index.ts
@@ -17,7 +17,7 @@ const constants = {
    invader : {
         dimensions: { width: 50, height: 50 }
     },
-    bullets: {
+    bullet: {
         dimensions: { width: 5, height: 10 }
     },
     scene: {
@@ -29,7 +29,7 @@ const state = {
     score: 0,
     lives: 3,       
     level: 1,
-    difficulty: 'easy',
+    difficulty: "easy",
     ship: { 
         position: { x: 400, y: 100 }
     },
@@ -62,13 +62,7 @@ const state = {
     }, {
         position: { x: 700, y: 400 }
     }],
-    bulletsPosition: [{
-        position: { x: 15, y: 15 },
-        type: 'ship'
-    }, {
-        position: { x: 25, y: 25 },
-        type: 'invader'
-    }],
+    bullets: [] as { position: {x: number, y: number} }[],
     gameOver: false,
 }
 
@@ -84,7 +78,7 @@ Logic
 - reset game
 */
 
-function checkCollision() {
+function checkCollisionShipVsInvaders() {
     const shipHalfWidth = constants.ship.dimensions.width / 2;
     const shipHalfHeight = constants.ship.dimensions.height / 2;    
     const invaderHalfWidth = constants.invader.dimensions.width / 2;
@@ -97,7 +91,7 @@ function checkCollision() {
         bottomLeft: { x: state.ship.position.x - shipHalfWidth, y: state.ship.position.y - shipHalfHeight }
     }
 
-    return state.invaders.some(invader => {
+    const collision = state.invaders.some(invader => {
         const invaderVertices = {
             topLeft: { x:invader.position.x - invaderHalfWidth, y: invader.position.y + invaderHalfHeight },
             topRight: { x: invader.position.x + invaderHalfWidth, y: invader.position.y + invaderHalfHeight },
@@ -112,73 +106,144 @@ function checkCollision() {
         
     })
 
+    if (collision) {
+        state.lives -= 1
+        if (state.lives <= 0) {
+            state.gameOver = true
+            alert('Game Over')
+        } else {
+            alert(`You have ${state.lives} lives left.`)
+        }
+    }
+
+}
+
+function checkCollisionBulletVsInvaders() {
+    const bulletHalfWidth = constants.bullet.dimensions.width / 2
+    const bulletHalfHeight = constants.bullet.dimensions.height / 2
+    const invaderHalfWidth = constants.invader.dimensions.width / 2
+    const invaderHalfHeight = constants.invader.dimensions.height / 2
+
+    return state.bullets.forEach(bullet => {
+        const bulletVertices = {
+            topLeft: { x: bullet.position.x - bulletHalfWidth, y: bullet.position.y + bulletHalfHeight },
+            topRight: { x: bullet.position.x + bulletHalfWidth, y: bullet.position.y + bulletHalfHeight },
+            bottomRight: { x: bullet.position.x + bulletHalfWidth, y: bullet.position.y - bulletHalfHeight },
+            bottomLeft: { x: bullet.position.x - bulletHalfWidth, y: bullet.position.y - bulletHalfHeight }
+        }
+
+        state.invaders = state.invaders.filter(invader => {
+            const invaderVertices = {
+                topLeft: { x: invader.position.x - invaderHalfWidth, y: invader.position.y + invaderHalfHeight },
+                topRight: { x: invader.position.x + invaderHalfWidth, y: invader.position.y + invaderHalfHeight },
+                bottomRight: { x: invader.position.x + invaderHalfWidth, y: invader.position.y - invaderHalfHeight },
+                bottomLeft: { x: invader.position.x - invaderHalfWidth, y: invader.position.y - invaderHalfHeight }
+            }
+
+            const collision = bulletVertices.topLeft.x <= invaderVertices.bottomRight.x &&
+                bulletVertices.bottomRight.x >= invaderVertices.topLeft.x && 
+                bulletVertices.topLeft.y >= invaderVertices.bottomRight.y &&
+                bulletVertices.bottomRight.y <= invaderVertices.topLeft.y
+
+            if (collision) {
+                state.score += 10
+
+                invadersElements.forEach((invaderElement, index) => {
+                    const invaderIndex = state.invaders.indexOf(invader)
+                    if (index === invaderIndex) {
+                        sceneElement.removeChild(invaderElement)
+                        invadersElements.splice(index, 1)
+                    }
+                })
+
+                bulletsElements.forEach((bulletElement, index) => {
+                    const bulletIndex = state.bullets.indexOf(bullet)
+
+                    if (index === bulletIndex) {
+                        sceneElement.removeChild(bulletElement)
+                        bulletsElements.splice(index, 1)
+                        state.bullets.splice(bulletIndex, 1)
+                    }
+                })
+            } 
+            
+            return !collision
+        })
+    })
 }
 
 /* Interface */
 
-const scene = document.getElementById('scene') as HTMLDivElement
-scene.style.position = 'relative'
-scene.style.width = '800px'
-scene.style.height = '600px'
-scene.style.backgroundColor = 'gray'
-//scene.style.overflow = 'hidden'
+const sceneElement = document.getElementById("scene") as HTMLDivElement
+sceneElement.style.position = "relative"
+sceneElement.style.width = "800px"
+sceneElement.style.height = "600px"
+sceneElement.style.backgroundColor = "gray"
 
-const ship = document.createElement('div')
-ship.style.position = 'absolute'
-ship.style.width = `${constants.ship.dimensions.width}px`
-ship.style.height = `${constants.ship.dimensions.height}px`
-ship.style.backgroundImage = 'url(./public/images/ship.png)'
-ship.style.backgroundSize = 'cover'
+const shipElement = document.createElement("div")
+shipElement.style.position = "absolute"
+shipElement.style.width = `${constants.ship.dimensions.width}px`
+shipElement.style.height = `${constants.ship.dimensions.height}px`
+shipElement.style.backgroundImage = "url(./public/images/ship.png)"
+shipElement.style.backgroundSize = "cover"
+shipElement.style.left = `${state.ship.position.x - constants.ship.dimensions.width / 2}px`
+shipElement.style.top = `${constants.scene.dimensions.height - (state.ship.position.y + constants.ship.dimensions.height / 2)}px`
 
-const invaders  = state.invaders.map(invader => {
-    const invaderElement = document.createElement('div') as HTMLDivElement
-    invaderElement.style.position = 'absolute'
+const invadersElements  = state.invaders.map(invader => {
+    const invaderElement = document.createElement("div") as HTMLDivElement
+    invaderElement.style.position = "absolute"
     invaderElement.style.width = `${constants.invader.dimensions.width}px`
     invaderElement.style.height = `${constants.invader.dimensions.height}px`
-    invaderElement.style.backgroundImage = 'url(./public/images/invader.png)'
-    invaderElement.style.backgroundSize = 'cover'
+    invaderElement.style.backgroundImage = "url(./public/images/invader.png)"
+    invaderElement.style.backgroundSize = "cover"
     invaderElement.style.left = `${invader.position.x - constants.invader.dimensions.width /2}px`
     invaderElement.style.top = `${constants.scene.dimensions.height - (invader.position.y + constants.invader.dimensions.height /2)}px`
     return invaderElement
 })
 
-scene.appendChild(ship)
-invaders.forEach(invader => scene.appendChild(invader))
+sceneElement.appendChild(shipElement)
+invadersElements.forEach(invader => sceneElement.appendChild(invader))
 
-document.addEventListener('keydown', event => {
+const bulletsElements = [] as HTMLDivElement[]
+
+document.addEventListener("keyup", event => {
     const step = 10
 
-    if (event.key === 'ArrowLeft') {
+    if (event.key === "ArrowLeft") {
         state.ship.position.x = Math.max(state.ship.position.x - step, constants.ship.dimensions.width / 2)
-    } else if (event.key === 'ArrowRight') {
+    } else if (event.key === "ArrowRight") {
         state.ship.position.x = Math.min(state.ship.position.x + step, constants.scene.dimensions.width - constants.ship.dimensions.width / 2)
-    } else if (event.key === 'ArrowUp') {
+    } else if (event.key === "ArrowUp") {
         state.ship.position.y = Math.min(state.ship.position.y + step, constants.scene.dimensions.height - constants.ship.dimensions.height / 2)
-    } else if (event.key === 'ArrowDown') {
+    } else if (event.key === "ArrowDown") {
         state.ship.position.y = Math.max(state.ship.position.y - step, constants.ship.dimensions.height / 2)
     }
 
-    ship.style.left = `${state.ship.position.x - constants.ship.dimensions.width / 2}px`
-    ship.style.top = `${constants.scene.dimensions.height - (state.ship.position.y + constants.ship.dimensions.height / 2)}px`
+    shipElement.style.left = `${state.ship.position.x - constants.ship.dimensions.width / 2}px`
+    shipElement.style.top = `${constants.scene.dimensions.height - (state.ship.position.y + constants.ship.dimensions.height / 2)}px`
 
-    gameLoop()
+    if (event.key === " ") {
+        const bullet = {
+            position: { x: state.ship.position.x, y: state.ship.position.y + constants.ship.dimensions.height / 2 + constants.bullet.dimensions.height / 2 }
+        }
+
+        state.bullets.push(bullet)
+
+        const bulletElement = document.createElement("div")
+        bulletElement.style.position = "absolute"
+        bulletElement.style.width = `${constants.bullet.dimensions.width}px`
+        bulletElement.style.height = `${constants.bullet.dimensions.height}px`
+        bulletElement.style.backgroundColor = "yellow"
+        bulletElement.style.left = `${bullet.position.x - constants.bullet.dimensions.width / 2}px`
+        bulletElement.style.top = `${constants.scene.dimensions.height - (bullet.position.y + constants.bullet.dimensions.height / 2)}px`
+
+        sceneElement.appendChild(bulletElement)
+        bulletsElements.push(bulletElement)
+    }
+
 })    
 
-function gameLoop() {
-    if (checkCollision()) {
-        console.log('Game Over')
-        state.gameOver = true
-        alert('Game Over')
-        // Reset game
-        //state.score = 0
-        //state.lives = 3
-        //state.level = 1
-        //state.ship.position = { x: constants.scene.dimensions.width / 2, y: constants.ship.dimensions.height / 2 }
-        //ship.style.left = `${state.ship.position.x - constants.ship.dimensions.width / 2}px`
-        //ship.style.top = `${constants.scene.dimensions.height - (state.ship.position.y + constants.//ship.dimensions.height / 2)}px`  
-        //state.gameOver = false
-    }
-}
+// Game Loop
 
 setInterval(() => {
     if (state.gameOver) return
@@ -190,15 +255,33 @@ setInterval(() => {
             invader.position.y = constants.scene.dimensions.height - constants.invader.dimensions.height / 2
         }
 
-        return invader
-    })
-
-    invaders.forEach((invaderElement, index) => {
-        const invader = state.invaders[index]!
-
+        const invaderIndex = state.invaders.indexOf(invader)
+        const invaderElement = invadersElements[invaderIndex]!
         invaderElement.style.left = `${invader.position.x - constants.invader.dimensions.width / 2}px`
         invaderElement.style.top = `${constants.scene.dimensions.height - (invader.position.y + constants.invader.dimensions.height / 2)}px`
+
+        return invader
+        
     })
 
-    gameLoop()
+    state.bullets = state.bullets.map((bullet, index) => {
+        bullet.position.y += 10
+
+        if (bullet.position.y - constants.bullet.dimensions.height / 2 > constants.scene.dimensions.height) {
+            state.bullets.splice(index, 1)
+            const bulletElement = bulletsElements[index]!
+            sceneElement.removeChild(bulletElement)
+            bulletsElements.splice(index, 1)
+        } else {
+            const bulletElement = bulletsElements[index]!
+            bulletElement.style.left = `${bullet.position.x - constants.bullet.dimensions.width / 2}px`
+            bulletElement.style.top = `${constants.scene.dimensions.height - (bullet.position.y + constants.bullet.dimensions.height / 2)}px`
+        }
+        return bullet
+    })
+
+    checkCollisionShipVsInvaders()
+    checkCollisionBulletVsInvaders()
+
+    
 }, 200);
